be28c756edf9c25c0108e4d928484cb533f9bcda
Stozo <35351828+Stozo@users.noreply.github.com>
2024-11-07 22:43:59 +0100

    blackjack now functional in file Functional_blackjack_don't_touch.py contiuned optimization in testgrounds.py

--- complet.py
+++ complet.py
@@ -1,4 +1,6 @@
+# imports
 import random
+import time
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -24,24 +26,23 @@ def draw_card(to_inv, __cards):
 
 # ___ Show hands ___
 def show_player_hands(hand):
+    print(hand)
+    """
     for i in range(len(hand)):
         rank, suit = hand[i]
         if rank != 10:
-            print(f"┌─────────┐\n"
-                  f"│{rank}        │\n"
-                  f"│         │\n"
-                  f"│    {suit}    │\n"
-                  f"│         │\n"
-                  f"│       {rank} │\n"
-                  f"└─────────┘")
+            print(f"┌─────┐\n"
+                  f"│{rank}    │\n"
+                  f"│  {suit}  │\n"
+                  f"│   {rank} │\n"
+                  f"└─────┘")
         else:
-            print(f"┌─────────┐\n"
-                  f"│{rank}       │\n"
-                  f"│         │\n"
-                  f"│    {suit}    │\n"
-                  f"│         │\n"
-                  f"│       {rank}│\n"
-                  f"└─────────┘")
+            print(f"┌─────┐\n"
+                  f"│{rank}   │\n"
+                  f"│  {suit}  │\n"
+                  f"│   {rank}│\n"
+                  f"└─────┘")
+    """
 
 # ___ Check sum ___
 def check_sum(hand):
@@ -59,19 +60,53 @@ def check_sum(hand):
             total_sum -= 10
     return total_sum
 
+# ___ Placing bet ___
+def place_bet():
+    global chips
+    while True:
+        if chips == 1:
+            print("You currently have 1 chip.")
+        else:
+            print(f"You currently have {chips} chips.")
 
-# ___ Game start ___
+        global bet_amount
+        bet_amount = int(input("How many chips do you want to bet?\n\n>"))
+
+        if bet_amount > chips:
+            print("You do not have enough chips for that bet. Try again.")
+        elif bet_amount <= 0:
+            print("Invalid bet amount. Try again.")
+        elif bet_amount >= 0 and bet_amount <= chips:
+            chips -= bet_amount
+            break
+
+    # ___ Game start ___
+
+# ___ Game initializer ___
+def start_game():
+
 
 while True:
+
     player_hand = []
     dealer_hand = []
+    chips = 10
 
     cards = create_new_deck()
     shuffle_deck(cards)
 
     print("Welcome to Blackjack")
 
+    place_bet()
+    print(f"\nYou placed a bet of {bet_amount} chips")
+    print("Shuffling...\n")
+    time.sleep(1)
     draw_card(player_hand, cards)
     draw_card(dealer_hand, cards)
     draw_card(player_hand, cards)
     draw_card(dealer_hand, cards)
+    show_player_hands(f"Players hand:\n{player_hand}\n")
+    show_player_hands(f"Dealers hand:\n{dealer_hand[1]}, (UNKNOWN)\n")
+    draw_card(dealer_hand, cards)
+    show_player_hands(f"Dealers hand:\n{dealer_hand[1]}, (UNKNOWN)\n")
+    break
